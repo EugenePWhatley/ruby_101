b519d64ed180b3ff89000608971496a0f6b982dd
Eugene Whatley <ewhatley@thoughtworks.com>
2021-07-06 11:28:15 -0400

    Intro to data types

---
+++ data_types.rb
@@ -0,0 +1,10 @@
+name = "Mike" 						# String
+age = 70 							# Integer
+gpa = 3.2 							# Float
+is_male = true						# Boolean
+flaws = nil							# No Value
+list = ["abc", 123, false, flaws]	# Array
+hash = { "one" => 1, "two" => 3}	# Dictionary
+
+puts list
+puts hash
