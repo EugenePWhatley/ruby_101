b9efe2f7fb76d6789177ff0514e87ba5e1327bce
Eugene Whatley <ewhatley@thoughtworks.com>
2021-07-06 12:25:57 -0400

    Intro to hashes

---
+++ hashes.rb
@@ -0,0 +1,11 @@
+state_codes = {
+ "New York" 	=> "NY",
+ :California 	=> "CA",
+ 'Ohio'			=> "OH",
+ 4 				=> "N/A"
+}
+
+puts state_codes
+puts state_codes["New York"]
+puts state_codes[:California]
+puts state_codes[4]
