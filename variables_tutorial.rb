97936a435c72765a14f8b37951ff4ba3ff7092ea
Eugene Whatley <ewhatley@thoughtworks.com>
2021-07-06 11:24:28 -0400

    Intro to variables

---
+++ variables_tutorial.rb
@@ -0,0 +1,10 @@
+age = 35
+name = "John"
+
+puts "There once was a man named #{name}"
+puts "he was #{age} years old."
+
+name = "Tom"
+
+puts "He really liked the name #{name}"
+puts "but didn't like being #{age}."
