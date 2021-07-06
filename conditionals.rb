7d9be81910c65a5c0e23780a6cac4f6d05953595
Eugene Whatley <ewhatley@thoughtworks.com>
2021-07-06 13:37:11 -0400

    Updated bitwise AND to conditional AND in previous example; Introduce slightly more complex conditionals

--- conditionals.rb
+++ conditionals.rb
@@ -1,11 +1,11 @@
 is_male = false
 is_tall = false
 
-if is_male & is_tall
+if is_male && is_tall
   puts "You are a tall male"
 elsif is_male and !is_tall
   puts "You are a short male"
-elsif !is_male & is_tall
+elsif !is_male && is_tall
   puts "You are not male but tall"
 else
   puts "You are not male or tall"
@@ -18,3 +18,16 @@ end
 if is_male or is_tall
   puts "You are either tall or male"
 end
+
+def max(num1, num2, num3)
+  if num1 >= num2 && num1 >= num3
+    return num1
+  elsif num2 >= num1 and num2 >= num3
+    return num2
+  else
+    return num3
+  end
+end
+
+puts max(3, 2, 1)
+
