042c1d4fd535e37dcfcab8e4c43e609bd3e9e092
Eugene Whatley <ewhatley@thoughtworks.com>
2021-07-06 11:46:47 -0400

    Intro to numbers

---
+++ numbers.rb
@@ -0,0 +1,22 @@
+# Arithmetic
+puts 5 + 9
+puts 2**3
+puts 15^3
+
+# Methods
+num = -30.23
+puts num.to_s
+puts num.abs
+puts num.round
+puts num.ceil
+puts num.floor
+
+# Math class
+puts Math.sqrt(36)
+puts Math.log(1)
+
+# Integer vs Floats
+puts 1 + 7
+puts 1 + 7.0
+puts 10 / 7
+puts 10.0 / 7
