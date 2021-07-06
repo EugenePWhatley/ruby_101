6d2e4ee4a2257b93e824ae0d32dd2ab9485df754
Eugene Whatley <ewhatley@thoughtworks.com>
2021-07-06 12:21:29 -0400

    Intro to arrays

---
+++ arrays.rb
@@ -0,0 +1,15 @@
+friends = Array["Kevin", "Tyler", "Oscar", "Angela"]
+puts friends
+puts friends[0]
+puts friends[-1]
+puts friends[0,1]
+friends[0] = "Karen"
+puts friends.length
+puts friends.include? "Tyler"
+puts friends.reverse
+puts friends.sort
+
+enemies = Array.new
+enemies[0] = "Johnny"
+enemies[5] = "Jess"
+puts enemies
